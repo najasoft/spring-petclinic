3817a003a06d5c9f37f6c195a23629fcc2d658a4
najasoft <nadiri@najasoft.com>
2021-04-10 21:46:51 +0100

    Peut être une erreur

--- src/main/java/org/springframework/samples/petclinic/model/Person.java
+++ src/main/java/org/springframework/samples/petclinic/model/Person.java
@@ -27,13 +27,14 @@ import javax.validation.constraints.NotEmpty;
 @MappedSuperclass
 public class Person extends BaseEntity {
 
+
 	@Column(name = "first_name")
 	@NotEmpty
 	private String firstName;
 
 	@Column(name = "last_name")
 	@NotEmpty
-	private String lastName;
+private String lastName;
 
 	public String getFirstName() {
 		return this.firstName;
